086202f31b6657d4852ab779c293d9fb46163b24
Ni Komang Cahya Sari <gcahya908@gmail.com>
2026-04-07 14:35:35 +0700

    menambahkan button cek dan membuat perhitungan jam

--- app/src/main/java/com/nikomangcahyasari0057/assesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/nikomangcahyasari0057/assesment1/ui/screen/MainScreen.kt
@@ -1,14 +1,19 @@
 package com.nikomangcahyasari0057.assesment1.ui.screen
 
 import android.content.res.Configuration
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
+import androidx.compose.material3.Button
 import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuAnchorType
@@ -22,11 +27,13 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.KeyboardType
 import androidx.compose.ui.tooling.preview.Preview
@@ -61,16 +68,17 @@ fun MainScreen() {
 fun ScreenContent(modifier: Modifier = Modifier) {
 
     var jam by remember { mutableStateOf("") }
+    var hasil by remember { mutableIntStateOf(0) }
 
     val aktivitasList = listOf(
         stringResource(R.string.sosial_media),
         stringResource(R.string.game),
-        stringResource(R.string.belajar)
+        stringResource(R.string.nonton_film)
     )
 
     var aktivitas by remember { mutableStateOf("") }
-
     var expanded by remember { mutableStateOf(false) }
+
 
     Column(
         modifier = modifier
@@ -126,8 +134,68 @@ fun ScreenContent(modifier: Modifier = Modifier) {
                 }
             }
         }
+
+        Button(
+            onClick = {
+                val jamInt = jam.toIntOrNull() ?: 0
+
+                hasil = when {
+                    jamInt < 3 -> 1
+                    jamInt in 3..6 -> 2
+                    else -> 3
+                }
+            },
+            modifier = Modifier.padding(top = 8.dp),
+            contentPadding = PaddingValues(horizontal = 32.dp, vertical = 16.dp)
+        ){
+            Text(stringResource(R.string.tombol_cek))
+        }
+
+        if (hasil != 0) {
+
+           val (hasilText, bgColor) = when (hasil) {
+               1 -> Pair(
+                   stringResource(R.string.normal),
+                   Color(0xFFD4EDDA)
+               )
+               2 -> Pair(
+                   stringResource(R.string.waspada),
+                   Color(0xFFFFF3CD)
+               )
+               else -> Pair(
+                   stringResource(R.string.kecanduan),
+                   Color(0xFFF8D7DA)
+               )
+           }
+
+            Box(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(vertical = 12.dp)
+                    .height(1.dp)
+                    .background(Color.Gray.copy(alpha = 0.7f))
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 16.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
+            ) {
+                Text(
+                    text = "Hasil Analisis",
+                    modifier = Modifier.padding(top = 16.dp),
+                )
+                Text(
+                    text = hasilText,
+                    style = MaterialTheme.typography.headlineLarge, // lebih besar
+                    modifier = Modifier.padding(top = 12.dp)
+                )
+            }
+        }
     }
 }
+
 
 @Preview(showBackground = true)
 @Preview(uiMode = Configuration.UI_MODE_NIGHT_YES, showBackground = true)
