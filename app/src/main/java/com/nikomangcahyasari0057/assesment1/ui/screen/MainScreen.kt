77ba844f2d07aba2c650a51ecd195bf9e18f83d9
Ni Komang Cahya Sari <gcahya908@gmail.com>
2026-04-08 00:13:34 +0700

    Membuat Tampilan Gambar

--- app/src/main/java/com/nikomangcahyasari0057/assesment1/ui/screen/MainScreen.kt
+++ app/src/main/java/com/nikomangcahyasari0057/assesment1/ui/screen/MainScreen.kt
@@ -3,12 +3,14 @@ package com.nikomangcahyasari0057.assesment1.ui.screen
 import android.content.Context
 import android.content.Intent
 import android.content.res.Configuration
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.rememberScrollState
 import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.foundation.verticalScroll
@@ -38,7 +40,9 @@ import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.text.input.KeyboardType
@@ -85,6 +89,8 @@ fun MainScreen(navController: NavHostController) {
 @Composable
 fun ScreenContent(modifier: Modifier = Modifier) {
 
+    val gambar = R.drawable.hp
+
     var jam by rememberSaveable { mutableStateOf("") }
     var jamError by rememberSaveable { mutableStateOf(false) }
 
@@ -111,6 +117,12 @@ fun ScreenContent(modifier: Modifier = Modifier) {
         horizontalAlignment = Alignment.CenterHorizontally
     ) {
 
+        Image(
+            painter = painterResource(id = gambar),
+            contentDescription = stringResource(R.string.deskripsi),
+            contentScale = ContentScale.Crop,
+            modifier = Modifier.size(200.dp)
+        )
         Text(
             text = stringResource(id = R.string.intro),
             style = MaterialTheme.typography.bodyLarge,
